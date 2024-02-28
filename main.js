cc8f70d21274c75dbcaf50034a15620b7f337d45
Resh1808 <161630580+Resh1808@users.noreply.github.com>
2024-02-29 01:29:51 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Jangan lupa makan!! Jangan sakit :)";
+  gif.src ="eating.gif";
+
+  yesBtn.addEventListener("click", () =>{
+    question.innerHTML = "Get plenty of rest too!!";
+    gif.src ="sleepy.gif";
+  
+  
+    yesBtn.addEventListener("click", () =>{
+      question.innerHTML = "Good luck kayy!";
+      gif.src ="fired.gif";
+    });
+
+  })
+
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
